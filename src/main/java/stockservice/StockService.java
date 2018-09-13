530dd77ecdc25f20fa4e3565d2f9664aa1af89af
Christopher Stafford <chris@chrisstafford.io>
2018-09-13 15:49:50 -0500

    Cleans up license template text

--- src/main/java/stockservice/StockService.java
+++ src/main/java/stockservice/StockService.java
@@ -1,13 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package stockservice;
 
 /**
- *
- * @author chris
+ * Interface for accessing StockQuotes from a given service
+ * 
+ * @author Christopher Stafford
  */
 public interface StockService {
     /**
